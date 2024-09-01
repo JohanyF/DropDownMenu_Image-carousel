8937afc9b2b46512f447616d5b0f2f351491dd7f
JohanyF <johanyf627@gmail.com>
2024-09-01 01:26:12 -0500

    Added functionality to dropdown btn to reveal the link when button is clicked on

--- src/index.js
+++ src/index.js
@@ -1,33 +1,8 @@
 import "./style.css";
-import setUpHomePage from "./home.js";
-import setUpMenuPage from "./menu.js";
-import setUpAboutPage from "./about.js";
 
-setUpHomePage();
+const dropDownBtn = document.querySelector(".dropbtn");
 
-const removeMainChild = () => {
-  const main = document.querySelector(".main");
-  const child = main.lastElementChild;
-  child.remove();
-};
-
-const homeBtn = document.querySelector("#home");
-
-homeBtn.addEventListener("click", () => {
-  removeMainChild();
-  setUpHomePage();
-});
-
-const menuBtn = document.querySelector("#menu");
-
-menuBtn.addEventListener("click", () => {
-  removeMainChild();
-  setUpMenuPage();
-});
-
-const aboutBtn = document.querySelector("#about");
-
-aboutBtn.addEventListener("click", () => {
-  removeMainChild();
-  setUpAboutPage();
+dropDownBtn.addEventListener("click", () => {
+  const dropDownItems = document.querySelector(".dropdown-items");
+  dropDownItems.classList.toggle("none");
 });
