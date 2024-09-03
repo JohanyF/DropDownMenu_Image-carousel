4510ed6561be361db2e6c7fcb0399fed18f18d11
JohanyF <johanyf627@gmail.com>
2024-09-03 01:25:12 -0500

    Added functionality to the navigation dots at the bottom of the slides. Also, added setInterval() that advances the slides every 5 seconds

--- src/index.js
+++ src/index.js
@@ -8,14 +8,17 @@ const photosArr = [
   {
     alt: "Brown Chihuahua/Pomeranian Mix Dog named Choco",
     src: Choco1,
+    id: 0,
   },
   {
     alt: "Brown Chihuahua/Pomeranian Mix Dog named Choco laying down relaxing",
     src: Choco2,
+    id: 1,
   },
   {
     alt: "Brown Chihuahua/Pomeranian Mix Dog named Choco with dirt on his nose",
     src: Choco3,
+    id: 2,
   },
 ];
 
@@ -26,13 +29,15 @@ dropDownBtn.addEventListener("click", () => {
   dropDownItems.classList.toggle("none");
 });
 
-const test = (photosArr) => {
+const imageCarousel = (photosArr) => {
   let currentIndex = 0;
+  const circleBtns = document.querySelectorAll(".circle-btn");
   const imgSlides = document.querySelector(".img-slides");
 
   const dogPic = new Image();
   dogPic.src = photosArr[0].src;
   dogPic.alt = photosArr[0].alt;
+  circleBtns[0].classList.add("selected");
 
   imgSlides.appendChild(dogPic);
 
@@ -46,6 +51,7 @@ const test = (photosArr) => {
 
     dogPic.src = photosArr[currentIndex].src;
     dogPic.alt = photosArr[currentIndex].alt;
+    selectedPhoto(photosArr[currentIndex].id, circleBtns);
   });
 
   const nextBtn = document.querySelector("#next");
@@ -58,19 +64,38 @@ const test = (photosArr) => {
 
     dogPic.src = photosArr[currentIndex].src;
     dogPic.alt = photosArr[currentIndex].alt;
+    selectedPhoto(photosArr[currentIndex].id, circleBtns);
   });
-
-  const circleBtns = document.querySelectorAll(".circle-btn");
 
   circleBtns.forEach((circleBtn) => {
     circleBtn.addEventListener("click", (event) => {
       currentIndex = event.target.id;
       dogPic.src = photosArr[currentIndex].src;
       dogPic.alt = photosArr[currentIndex].alt;
+      selectedPhoto(photosArr[currentIndex].id, circleBtns);
     });
   });
 
-  // TODO: Add a timeout which advances the slides every 5 seconds
+  setInterval(() => {
+    currentIndex++;
+
+    if (currentIndex > photosArr.length - 1) {
+      currentIndex = 0;
+    }
+
+    dogPic.src = photosArr[currentIndex].src;
+    dogPic.alt = photosArr[currentIndex].alt;
+    selectedPhoto(photosArr[currentIndex].id, circleBtns);
+  }, 5000);
+
+  const selectedPhoto = (photoID, circleBtns) => {
+    circleBtns.forEach((circleBtn) => {
+      circleBtn.classList.remove("selected");
+      if (parseInt(circleBtn.id, 10) === photoID) {
+        circleBtn.classList.add("selected");
+      }
+    });
+  };
 };
 
-test(photosArr);
+imageCarousel(photosArr);
